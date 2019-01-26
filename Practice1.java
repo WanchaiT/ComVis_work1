b2dde05732036549e63f8d319a677130e3d3d796
Wanchai Thiantanawat <wanchai.thiantanawat@hotmail.com>
2019-01-26 16:00:56 +0700

    Finish

--- Practice1.java
+++ Practice1.java
@@ -28,17 +28,25 @@ class Practice1{
     public static void writeImage(BufferedImage img ,String path){
         String[] destPath = path.split("\\.");
         //System.out.println(toPath[1]);
+        int index = destPath[0].lastIndexOf("/");
+        String folder = path.substring(0 ,index);
+        File outTest = new File(folder);
+
+        if (!outTest.exists()) {
+            System.out.println("Error writing image 1");
+            return;
+        }
 
         try {
             //System.out.println("destPath " + toPath[0] + "." + toPath[1]);
             File outFile = new File(path);
-
-            if (!outFile.exists()) {
+            ImageIO.write(img, destPath[1] ,outFile);
+            /*if (folder[0].exists()) {
                 ImageIO.write(img, destPath[1] ,outFile);
             }else {
                 System.out.println("Error writing image 1");
                 return;
-            }
+            }*/
 
         } catch(IOException e) {
             System.err.println("Error writing image");
